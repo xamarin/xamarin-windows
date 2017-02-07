25cc93eab98133c7bd4cc575025ad99f48c0144f
Niklas Therning <niklas@therning.org>
2017-02-07 16:54:55 +0100

    Added initial Visual Studio extension including ConsoleApplication and StaticLibrary templates. So far these templates don't do anything Xamarin specific.

---
+++ vs/Xamarin.Windows.VisualStudio.Templates/CSharp/ConsoleApplication/Program.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace $safeprojectname$
+{
+	public class Program
+	{
+		static void Main(string[] args)
+		{
+		}
+	}
+}
