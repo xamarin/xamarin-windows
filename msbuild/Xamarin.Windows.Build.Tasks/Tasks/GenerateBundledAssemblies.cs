a8107209929aa280c1a01d303581f9378a76ea4a
Niklas Therning <niklas@therning.org>
2017-04-26 15:29:48 +0200

    Use .bundle.c as suffix for generated files to avoid collision with object files generated by the AOT compiler.
    Added SkipUnchanged property which, when "true", avoids regenerating and
    recompiling bundle C files for assemblies which haven't changed (and config
    file hasn't changed).

--- msbuild/Xamarin.Windows.Build.Tasks/Tasks/GenerateBundledAssemblies.cs
+++ msbuild/Xamarin.Windows.Build.Tasks/Tasks/GenerateBundledAssemblies.cs
@@ -21,6 +21,8 @@ namespace Xamarin.Windows.Tasks
 		[Required]
 		public string OutputDirectory { get; set; }
 
+		public bool SkipUnchanged { get; set; }
+
 		[Output]
 		public ITaskItem[] GeneratedFiles { get; set; }
 
@@ -42,6 +44,7 @@ namespace Xamarin.Windows.Tasks
 			Log.LogDebugMessage("GenerateBundledAssemblies Task");
 			Log.LogDebugTaskItems("  Assemblies:", Assemblies);
 			Log.LogDebugTaskItems("  ConfigFiles:", ConfigFiles);
+			Log.LogDebugMessage("  SkipUnchanged: {0}", SkipUnchanged);
 
 			if (!Directory.Exists(OutputDirectory)) {
 				Directory.CreateDirectory(OutputDirectory);
@@ -50,7 +53,7 @@ namespace Xamarin.Windows.Tasks
 			var outputFiles = new List<string>();
 			var bundledConfigFiles = new List<string>();
 			foreach (var assembly in Assemblies) {
-				var outputFile = Path.Combine(OutputDirectory, Path.GetFileName(assembly.ItemSpec) + ".c");
+				var outputFile = Path.Combine(OutputDirectory, Path.GetFileName(assembly.ItemSpec) + ".bundle.c");
 				string configFile;
 				GenerateBundledAssembly(assembly.ItemSpec, outputFile, out configFile);
 				outputFiles.Add(outputFile);
@@ -83,8 +86,25 @@ namespace Xamarin.Windows.Tasks
 
 		public void GenerateBundledAssembly(string assemblyFile, string outputFile, out string configFile)
 		{
-			Log.LogDebugMessage($"  Generating output '{outputFile}' from assembly '{assemblyFile}'");
 			var assemblyName = Symbols.GetBundledAssemblyName(assemblyFile, Log);
+			var assemblyConfigFileCopy = Path.Combine(Path.GetDirectoryName(outputFile), assemblyName) + ".config";
+			var configName = (assemblyName + ".config").ToLower();
+			try {
+				configFile = ConfigFiles.First(t => Path.GetFileName(t.ItemSpec ?? "").ToLower() == configName).ItemSpec;
+			} catch {
+				configFile = null;
+			}
+
+			if (SkipUnchanged && File.Exists(outputFile) && File.Exists(assemblyFile) 
+						&& File.GetLastWriteTime(outputFile) >= File.GetLastWriteTime(assemblyFile)) {
+				if (configFile == null && !File.Exists(assemblyConfigFileCopy)
+					|| configFile != null && File.Exists(assemblyConfigFileCopy) && File.GetLastWriteTime(configFile) >= File.GetLastWriteTime(assemblyConfigFileCopy)) {
+					Log.LogDebugMessage("  Not regenerating bundle file for unchanged assembly: {0}", assemblyFile);
+					return;
+				}
+			}
+
+			Log.LogDebugMessage($"  Generating output '{outputFile}' from assembly '{assemblyFile}'");
 			var bundledAssemblyGetter = Symbols.GetBundledAssemblyGetter(assemblyName);
 			var bundledAssemblyConfigGetter = Symbols.GetBundledAssemblyConfigGetter(assemblyName);
 			var bundledAssemblyCleanup = Symbols.GetBundledAssemblyCleanup(assemblyName);
@@ -98,21 +118,29 @@ namespace Xamarin.Windows.Tasks
 					outs.WriteLine($"const MonoBundledAssembly *{bundledAssemblyGetter} (void) {{ return &bundle; }}");
 
 					outs.WriteLine("typedef struct { const char* name; const char* data; } MonoBundledAssemblyConfig;");
-					try {
-						var configName = (assemblyName + ".config").ToLower();
-						configFile = ConfigFiles.First(t => Path.GetFileName(t.ItemSpec ?? "").ToLower() == configName).ItemSpec;
-						using (var cfgs = File.OpenRead(configFile)) {
-							Log.LogDebugMessage($"    Found assembly config file '{configFile}' for assembly '{assemblyFile}'");
-							outs.WriteLine("static const char config_data [] = {");
-							WriteFileAsCArray(cfgs, outs);
-							outs.WriteLine("0};");
-							outs.WriteLine($"static const MonoBundledAssemblyConfig config = {{\"{assemblyName}\", config_data}};");
+					if (configFile != null) {
+						try {
+							using (var cfgs = File.OpenRead(configFile)) {
+								Log.LogDebugMessage($"    Found assembly config file '{configFile}' for assembly '{assemblyFile}'");
+								outs.WriteLine("static const char config_data [] = {");
+								WriteFileAsCArray(cfgs, outs);
+								outs.WriteLine("0};");
+								outs.WriteLine($"static const MonoBundledAssemblyConfig config = {{\"{assemblyName}\", config_data}};");
+							}
+						} catch {
+							// Return NULL if the assembly has no config file.
+							configFile = null;
 						}
-					} catch {
-						// Return NULL if the assembly has no config file.
-						configFile = null;
+					}
+					if (configFile == null) {
 						Log.LogDebugMessage($"    No assembly config file found for assembly '{assemblyFile}'");
 						outs.WriteLine($"static const MonoBundledAssemblyConfig config = {{\"{assemblyName}\", 0L}};");
+					}
+					if (File.Exists(assemblyConfigFileCopy)) {
+						File.Delete(assemblyConfigFileCopy);
+					}
+					if (configFile != null) {
+						File.Copy(configFile, assemblyConfigFileCopy);
 					}
 					outs.WriteLine($"const MonoBundledAssemblyConfig *{bundledAssemblyConfigGetter} (void) {{ return &config; }}");
 
