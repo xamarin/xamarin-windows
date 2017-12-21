e1948c4eebd57391989851b768a478b9bddfa594
Niklas Therning <nitherni@microsoft.com>
2017-12-21 10:33:18 +0100

    Modified the VS extension to include the MSBuild toolchain and reference assemblies, which is supported since VS 2017.

--- vs/Xamarin.Windows.VisualStudio.Vsix/WindowsDebuggerSession.cs
+++ vs/Xamarin.Windows.VisualStudio.Vsix/WindowsDebuggerSession.cs
@@ -2,31 +2,46 @@
 using System;
 using Mono.Debugging.Client;
 using EnvDTE;
+using System.Diagnostics;
 using System.IO;
+using Microsoft.VisualStudio.Setup.Configuration;
 
 namespace Xamarin.Windows
 {
     class WindowsDebuggerSession : SoftDebuggerSession
     {
+        // The GUID of Microsoft.VisualStudio.Setup.Configuration.SetupConfigurationClass
+        private readonly Guid SetupConfigurationClassGuid = new Guid("177F0C4A-1CD3-4DE7-A32C-71DBBB9FA36D");
         private System.Diagnostics.Process process;
+        private string installPath = null;
 
         protected override void OnRun(DebuggerStartInfo startInfo)
         {
             base.OnRun(startInfo);
             UseOperationThread = true;
 
-            var exeName = GetOutputAssembly(((Mono.Debugging.VisualStudio.StartInfo)startInfo).StartupProject);
-            var monoDirectory = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ProgramFilesX86), @"MSBuild\Xamarin\Windows\x64\Release");
-            var monoPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ProgramFilesX86), @"Reference Assemblies\Microsoft\Framework\Xamarin.Windows\v1.0");
+            var assemblyPath = GetOutputAssembly(((Mono.Debugging.VisualStudio.StartInfo) startInfo).StartupProject);
+            var monoDirectory = Path.Combine(GetInstallPath(), @"MSBuild\Xamarin\Windows\x64\Release");
+            var monoPath = Path.Combine(GetInstallPath(), @"Common7\IDE\ReferenceAssemblies\Microsoft\Framework\Xamarin.Windows\v1.0");
 
             var args = ((Mono.Debugging.VisualStudio.StartInfo)startInfo).StartArgs as SoftDebuggerListenArgs;
 
             process = new System.Diagnostics.Process();
-            process.StartInfo = new System.Diagnostics.ProcessStartInfo(Path.Combine(monoDirectory, "mono-sgen.exe"), string.Format("--debug --debugger-agent=transport=dt_socket,address=127.0.0.1:{0} {1}", args.DebugPort, exeName));
-            process.StartInfo.WorkingDirectory = monoDirectory;
+            process.StartInfo = new ProcessStartInfo(Path.Combine(monoDirectory, "mono-sgen.exe"), $"--debug --debugger-agent=transport=dt_socket,address=127.0.0.1:{args.DebugPort} \"{assemblyPath}\"");
+            process.StartInfo.WorkingDirectory = Path.GetDirectoryName(assemblyPath);
             process.StartInfo.UseShellExecute = false;
             process.StartInfo.EnvironmentVariables["MONO_PATH"] = monoPath;
             process.Start();
+        }
+
+        private string GetInstallPath()
+        {
+            if (installPath == null) {
+                var config = (ISetupConfiguration2) Activator.CreateInstance(Type.GetTypeFromCLSID(SetupConfigurationClassGuid));
+                var instance = config.GetInstanceForCurrentProcess();
+                installPath = instance.GetInstallationPath();
+            }
+            return installPath;
         }
 
         private string GetOutputAssembly(Project startupProject)
@@ -35,7 +50,7 @@ namespace Xamarin.Windows
             var outFolder = startupProject.ConfigurationManager.ActiveConfiguration.Properties.Item("OutputPath").Value.ToString();
             var assemblyName = startupProject.Properties.Item("OutputFileName").Value.ToString();
 
-            return string.Format("\"{0}\"", Path.Combine(baseFolder, outFolder, assemblyName));
+            return Path.Combine(baseFolder, outFolder, assemblyName);
         }
 
         protected override void OnExit()
