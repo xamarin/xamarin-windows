80a56ec416633c15b64cb31dc42c36187f694dba
lateralusX <lateralusx.github@gmail.com>
2019-07-05 13:15:13 +0200

    Add support for parallel AOT build tasks.
    New AOT Task property, MaxParallelBuildTasks, handles parallelization of
    AOT build tasks to decrease build times. Defaults to 1 to keep current
    behavior. A value of 0 or -1 means current number of processors on the system.

--- msbuild/Xamarin.Windows.Build.Tasks/Tasks/Aot.cs
+++ msbuild/Xamarin.Windows.Build.Tasks/Tasks/Aot.cs
@@ -5,7 +5,6 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
-using System.Text;
 using System.Threading;
 using Microsoft.Build.Framework;
 using Microsoft.Build.Utilities;
@@ -41,6 +40,8 @@ namespace Xamarin.Windows.Tasks
 		public string OutputFileExtension { get; set; }
 		public string Runtime { get; set; }
 		public bool EnableLLVM {get; set; } = false;
+
+		public int MaxParallelBuildTasks { get; set; } = 1;
 
 		[Required]
 		public ITaskItem[] ResolvedFrameworkAssemblies { get; set; }
@@ -86,6 +87,8 @@ namespace Xamarin.Windows.Tasks
 
 		private bool DoExecute()
 		{
+			bool failureDetected = false;
+
 			if (string.IsNullOrEmpty(OutputFileExtension)) {
 				OutputFileExtension = DEFAULT_FILE_EXTENSIONS[(int) outputFileType];
 			}
@@ -126,98 +129,173 @@ namespace Xamarin.Windows.Tasks
 						.Select(p => p.TrimEnd('\\', '/'))
 						.Distinct(StringComparer.CurrentCultureIgnoreCase));
 
-			foreach (var assembly in ResolvedFrameworkAssemblies.Union(ResolvedUserAssemblies)) {
-				if (cancellationToken.IsCancellationRequested) {
-					return false;
-				}
+			int parallelBuildTasks = MaxParallelBuildTasks <= 0 ? Environment.ProcessorCount : MaxParallelBuildTasks;
+			if (parallelBuildTasks > 1)
+				Log.LogMessage(MessageImportance.High, "Using {0} parallel build tasks.", parallelBuildTasks);
 
-				var outputFile = Path.Combine(AotOutputDirectory, Path.GetFileName(assembly.ItemSpec) + "." + OutputFileExtension);
-				var outputFileLLVM = Path.Combine(AotOutputDirectory, Path.GetFileName(assembly.ItemSpec) + "-llvm." + OutputFileExtension);
-
-				outputFiles.Add(outputFile);
-				if (EnableLLVM) {
-					outputFiles.Add(outputFileLLVM);
-				}
-
-				var assemblyPath = Path.GetFullPath(assembly.ItemSpec);
-				if (OnlyRecompileIfChanged && File.Exists(outputFile) && File.Exists(assemblyPath) 
-						&& File.GetLastWriteTime(outputFile) >= File.GetLastWriteTime(assemblyPath)) {
-					if (!EnableLLVM && File.Exists(outputFileLLVM)) {
-						Log.LogMessage(MessageImportance.High, "  Found LLVM object file for assembly: {0} during none LLVM build, forcing recompile.", assemblyPath);
-					} else {
-						Log.LogMessage(MessageImportance.High, "  Not recompiling unchanged assembly: {0}", assemblyPath);
-						continue;
+			using (SemaphoreSlim parallellCompileTasks = new SemaphoreSlim(parallelBuildTasks))
+			{
+				var compileTasks = new List<System.Threading.Tasks.Task<bool>>();
+				var currentCompileTaskId = 0;
+				foreach (var assembly in ResolvedFrameworkAssemblies.Union(ResolvedUserAssemblies))
+				{
+					if (cancellationToken.IsCancellationRequested)
+					{
+						failureDetected = true;
+						break;
 					}
+
+					var outputFile = Path.Combine(AotOutputDirectory, Path.GetFileName(assembly.ItemSpec) + "." + OutputFileExtension);
+					var outputFileLLVM = Path.Combine(AotOutputDirectory, Path.GetFileName(assembly.ItemSpec) + "-llvm." + OutputFileExtension);
+
+					outputFiles.Add(outputFile);
+					if (EnableLLVM)
+					{
+						outputFiles.Add(outputFileLLVM);
+					}
+
+					var assemblyPath = Path.GetFullPath(assembly.ItemSpec);
+					if (OnlyRecompileIfChanged && File.Exists(outputFile) && File.Exists(assemblyPath)
+							&& File.GetLastWriteTime(outputFile) >= File.GetLastWriteTime(assemblyPath))
+					{
+						if (!EnableLLVM && File.Exists(outputFileLLVM))
+						{
+							Log.LogMessage(MessageImportance.High, "  Found LLVM object file for assembly: {0} during none LLVM build, forcing recompile.", assemblyPath);
+						}
+						else
+						{
+							Log.LogMessage(MessageImportance.High, "  Not recompiling unchanged assembly: {0}", assemblyPath);
+							continue;
+						}
+					}
+
+					if (File.Exists(outputFile))
+						File.Delete(outputFile);
+					if (File.Exists(outputFileLLVM))
+						File.Delete(outputFileLLVM);
+
+					var tempDir = Path.Combine(AotOutputDirectory, Path.GetFileName(assembly.ItemSpec) + ".tmp");
+					if (!Directory.Exists(tempDir))
+						Directory.CreateDirectory(tempDir);
+
+					var aotOptions = new List<string>();
+
+					if (!string.IsNullOrEmpty(PreAdditionalAotArguments))
+					{
+						aotOptions.Add(PreAdditionalAotArguments);
+					}
+					aotOptions.Add("full");
+					if (EnableLLVM)
+					{
+						aotOptions.Add("llvm");
+						aotOptions.Add("llvm-outfile=" + QuoteFileName(outputFileLLVM));
+					}
+					aotOptions.Add("outfile=" + QuoteFileName(outputFile));
+					if (IsDebug && ResolvedUserAssemblies.Contains(assembly))
+					{
+						aotOptions.Add("soft-debug");
+					}
+					if (GenerateNativeDebugInfo)
+					{
+						aotOptions.Add("dwarfdebug");
+					}
+					if (outputFileType == AotFileType.Asm)
+					{
+						aotOptions.Add("asmonly");
+					}
+					aotOptions.Add("print-skipped");
+					if (outputFileType != AotFileType.Dll)
+					{
+						aotOptions.Add("static");
+					}
+					aotOptions.Add("tool-prefix=" + QuoteFileName(NativeToolsPrefix));
+					aotOptions.Add("ld-flags=" + NativeLinkerFlags);
+					aotOptions.Add("temp-path=" + QuoteFileName(tempDir));
+					if (!string.IsNullOrEmpty(PostAdditionalAotArguments))
+					{
+						aotOptions.Add(PostAdditionalAotArguments);
+					}
+
+					var args = new List<string>();
+					if (IsDebug)
+					{
+						args.Add("--debug");
+					}
+					if (!string.IsNullOrWhiteSpace(Runtime))
+					{
+						args.Add("--runtime=" + Runtime);
+					}
+					args.Add("--aot=" + string.Join(",", aotOptions));
+					args.Add('"' + assemblyPath + '"');
+
+					parallellCompileTasks.Wait();
+					if (updateAndCheckTasksResult(compileTasks))
+					{
+						failureDetected = true;
+						break;
+					}
+
+					int capturedCompileTaskId = ++currentCompileTaskId;
+					var compileTask = System.Threading.Tasks.Task<bool>.Factory.StartNew(() => {
+						bool result = false;
+						try
+						{
+							string compileTaskName = String.Format("{0}", "[AOT-" + capturedCompileTaskId + "]");
+							Log.LogMessage(MessageImportance.High, "  {0} compiling assembly: {1}", compileTaskName, assemblyPath);
+							result = RunAotCompiler(AotCompilerPath, args, assembliesPath, compileTaskName);
+							if (!result)
+							{
+								Log.LogCodedError("XW3001", "Could not AOT compile the assembly: {0}", assemblyPath);
+							}
+							else
+							{
+								Log.LogMessage(MessageImportance.High, "  {0} compilation of {1} finished", compileTaskName, Path.GetFileName(assemblyPath));
+							}
+						}
+						finally
+						{
+							parallellCompileTasks.Release();
+						}
+						return result;
+					});
+
+					compileTasks.Add(compileTask);
 				}
 
-				if (File.Exists (outputFile))
-					File.Delete (outputFile);
-				if (File.Exists (outputFileLLVM))
-					File.Delete (outputFileLLVM);
-
-				var tempDir = Path.Combine(AotOutputDirectory, Path.GetFileName(assembly.ItemSpec) + ".tmp");
-				if (!Directory.Exists(tempDir))
-					Directory.CreateDirectory(tempDir);
-
-				var aotOptions = new List<string>();
-
-				if (!string.IsNullOrEmpty(PreAdditionalAotArguments)) {
-					aotOptions.Add(PreAdditionalAotArguments);
+				System.Threading.Tasks.Task.WaitAll(compileTasks.ToArray());
+				if (updateAndCheckTasksResult(compileTasks)) {
+					failureDetected = true;
 				}
-				aotOptions.Add("full");
-				if (EnableLLVM) {
-					aotOptions.Add("llvm");
-					aotOptions.Add("llvm-outfile=" + QuoteFileName(outputFileLLVM));
-				}
-				aotOptions.Add("outfile=" + QuoteFileName(outputFile));
-				if (IsDebug && ResolvedUserAssemblies.Contains(assembly)) {
-					aotOptions.Add("soft-debug");
-				}
-				if (GenerateNativeDebugInfo) {
-					aotOptions.Add("dwarfdebug");
-				}
-				if (outputFileType == AotFileType.Asm) {
-					aotOptions.Add("asmonly");
-				}
-				aotOptions.Add("print-skipped");
-				if (outputFileType != AotFileType.Dll) {
-					aotOptions.Add("static");
-				}
-				aotOptions.Add("tool-prefix=" + QuoteFileName(NativeToolsPrefix));
-				aotOptions.Add("ld-flags=" + NativeLinkerFlags);
-				aotOptions.Add("temp-path=" + QuoteFileName(tempDir));
-				if (!string.IsNullOrEmpty(PostAdditionalAotArguments)) {
-					aotOptions.Add(PostAdditionalAotArguments);
-				}
-
-				var args = new List<string>();
-				if (IsDebug) {
-					args.Add("--debug");
-				}
-				if (!string.IsNullOrWhiteSpace(Runtime)) {
-					args.Add("--runtime=" + Runtime);
-				}
-				args.Add("--aot=" + string.Join(",", aotOptions));
-				args.Add('"' + assemblyPath + '"');
-
-				Log.LogMessage(MessageImportance.High, "  AOT compiling assembly: {0}", assemblyPath);
-				if (!RunAotCompiler(AotCompilerPath, args, assembliesPath)) {
-					Log.LogCodedError("XW3001", "Could not AOT compile the assembly: {0}", assemblyPath);
-					return false;
-				}
-				Log.LogMessage(MessageImportance.High, "  AOT compilation of {0} finished", Path.GetFileName(assemblyPath));
 			}
 
-			GeneratedFiles = outputFiles.Select(f => new TaskItem(f)).ToArray();
-			Log.LogDebugTaskItems("  [Output] GeneratedFiles:", GeneratedFiles);
+			if (!failureDetected) {
+				GeneratedFiles = outputFiles.Select(f => new TaskItem(f)).ToArray();
+				Log.LogDebugTaskItems("  [Output] GeneratedFiles:", GeneratedFiles);
+			}
 
-			return true;
+			return !failureDetected;
 		}
 
-		bool RunAotCompiler(string aotCompiler, List<string> args, string assembliesPath)
+		bool updateAndCheckTasksResult (List<System.Threading.Tasks.Task<bool>> tasks)
+		{
+			bool hasFailure = false;
+
+			for (var i = tasks.Count - 1; i >= 0; i--) {
+				var currentTask = tasks[i];
+				if (currentTask.IsCompleted && currentTask.Result == true) {
+					tasks.RemoveAt(i);
+				} else if (currentTask.IsCompleted && currentTask.Result == false) {
+					hasFailure = true;
+				}
+			}
+
+			return hasFailure;
+		}
+
+		bool RunAotCompiler(string aotCompiler, List<string> args, string assembliesPath, string compileTaskName)
 		{
 			if (!File.Exists(Path.Combine(NativeToolchainPaths, "clang.exe"))) {
-				Log.LogError($"Could not find clang.exe in {NativeToolchainPaths}. "
+				Log.LogError($"    {compileTaskName}: Could not find clang.exe in {NativeToolchainPaths}. "
 							  + "Make sure to select the feature \"Clang with Microsoft Codegen\" in the Visual Studio Installer "
 							  + "under \"Cross Platform Mobile Development\"->\"Visual C++ Mobile Development\".");
 				return false;
@@ -226,12 +304,12 @@ namespace Xamarin.Windows.Tasks
 			var crossCompilerPath = Path.GetDirectoryName(aotCompiler);
 			if (EnableLLVM) {
 				if (!File.Exists(Path.Combine(crossCompilerPath, "opt.exe"))) {
-					Log.LogError($"Could not find opt.exe in {crossCompilerPath}.");
+					Log.LogError($"    {compileTaskName}: Could not find opt.exe in {crossCompilerPath}.");
 					return false;
 				}
 
 				if (!File.Exists(Path.Combine(crossCompilerPath, "llc.exe"))) {
-					Log.LogError($"Could not find llc.exe in {crossCompilerPath}.");
+					Log.LogError($"    {compileTaskName}: Could not find llc.exe in {crossCompilerPath}.");
 					return false;
 				}
 			}
@@ -251,36 +329,75 @@ namespace Xamarin.Windows.Tasks
 			psi.EnvironmentVariables["MONO_PATH"] = assembliesPath;
 			psi.EnvironmentVariables["PATH"] = NativeToolchainPaths + ";" + crossCompilerPath;
 
-			Log.LogMessage(MessageImportance.High, "    [AOT] PATH=\"{0}\" MONO_PATH=\"{1}\" MONO_ENV_OPTIONS=\"{2}\" {3} {4}",
+			Log.LogMessage(MessageImportance.High, "    {0}: PATH=\"{1}\" MONO_PATH=\"{2}\" MONO_ENV_OPTIONS=\"{3}\" {4} {5}",
+				compileTaskName,
 				psi.EnvironmentVariables["PATH"],
 				psi.EnvironmentVariables["MONO_PATH"],
 				psi.EnvironmentVariables["MONO_ENV_OPTIONS"],
 				psi.FileName,
 				psi.Arguments);
 
-			var proc = new Process();
+			var proc = new CustomProcess();
 			proc.OutputDataReceived += OnAotOutputData;
 			proc.ErrorDataReceived += OnAotErrorData;
 			proc.StartInfo = psi;
 			proc.Start();
 			proc.BeginOutputReadLine();
 			proc.BeginErrorReadLine();
-			cancellationToken.Token.Register(() => { try { proc.Kill(); } catch (Exception) { } });
+			proc.CompileTaskName = compileTaskName;
+
+			lock (cancellationToken) {
+				cancellationToken.Token.Register(() => {
+					try {
+						var tkStartInfo = new ProcessStartInfo {
+							FileName = "taskkill",
+							Arguments = $"/T /F /PID {proc.Id}",
+							RedirectStandardError = false,
+							RedirectStandardOutput = false,
+							UseShellExecute = false
+						};
+
+						Log.LogMessage(MessageImportance.High, $"    {compileTaskName}: Canceling aot-compiler, PID={proc.Id}");
+						var tkProcess = Process.Start(tkStartInfo);
+						if (tkProcess.WaitForExit(30000)) {
+							Log.LogMessage(MessageImportance.High, $"    {compileTaskName}: aot-compiler, PID={proc.Id}, canceled");
+						} else {
+							Log.LogError($"    {compileTaskName}: Canceling aot-compiler, PID={proc.Id}, timed out");
+							tkProcess.Kill();
+						}
+					} catch (Exception) { }
+				});
+			}
+			Log.LogMessage(MessageImportance.Normal, $"    {compileTaskName}: Executing aot-compiler, PID={proc.Id}");
 			proc.WaitForExit();
+			Log.LogMessage(MessageImportance.Normal, $"    {compileTaskName}: aot-compiler, PID={proc.Id}, completed with exit code={proc.ExitCode}");
+
 			return proc.ExitCode == 0;
 		}
-
 		void OnAotOutputData(object sender, DataReceivedEventArgs e)
 		{
+			string compileTaskName = "";
+			if (sender is CustomProcess)
+				compileTaskName = ((CustomProcess)sender).CompileTaskName;
+
 			if (e.Data != null)
-				Log.LogMessage(MessageImportance.High, "    [aot-compiler stdout] {0}", e.Data);
+				Log.LogMessage(MessageImportance.High, "    {0}[aot-compiler stdout]: {1}", compileTaskName, e.Data);
 		}
 
 		void OnAotErrorData(object sender, DataReceivedEventArgs e)
 		{
+			string compileTaskName = "";
+			if (sender is CustomProcess)
+				compileTaskName = ((CustomProcess)sender).CompileTaskName;
+
 			if (e.Data != null)
-				Log.LogMessage(MessageImportance.High, "    [aot-compiler stderr] {0}", e.Data);
+				Log.LogMessage(MessageImportance.High, "    {0}[aot-compiler stderr]: {1}", compileTaskName, e.Data);
 		}
 
+	}
+
+	class CustomProcess : Process
+	{
+		public string CompileTaskName { get; set; }
 	}
 }
